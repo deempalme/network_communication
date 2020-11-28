b3ed4712e0047c7f399a21108090329ad0f605b6
deempalme <20066249+deempalme@users.noreply.github.com>
2020-11-28 13:51:16 -0700

    Removed compilation flags

--- include/ramrod/network_communication/server.h
+++ include/ramrod/network_communication/server.h
@@ -1,6 +1,7 @@
 #ifndef RAMROD_NETWORK_COMMUNICATION_SERVER_H
 #define RAMROD_NETWORK_COMMUNICATION_SERVER_H
 
+#include <chrono>
 #include <cstdarg>
 #include <cstdio>
 #include <string>
@@ -17,7 +18,7 @@ namespace ramrod {
       server();
       ~server();
 
-      bool connect(const std::string ip = "127.1.0.13", const int port = 1313);
+      bool connect(const std::string ip, const int port = 1313);
       bool disconnect();
 
       void max_queue(const int max_pending_connections = 10);
@@ -26,20 +27,28 @@ namespace ramrod {
       uint32_t host_to_network(const uint32_t host_value);
 
       const std::string& ip();
+      bool is_connected();
 
-      short mtu();
-      void mtu(const short mtu_byte_size = 1500);
+      uint16_t mtu();
+      void mtu(const uint16_t mtu_byte_size);
 
       uint16_t network_to_host(const uint16_t network_value);
       uint32_t network_to_host(const uint32_t network_value);
 
       int port();
 
-      ssize_t receive(void *data, const std::size_t length);
+      ssize_t receive(char *data, const std::size_t length);
       ssize_t receive(std::string *data, std::size_t length = 0, std::size_t stride = 0);
 
-      ssize_t send(const void *data, const std::size_t length);
+      bool reconnect();
+
+      int time_to_reconnect();
+      void time_to_reconnect(const int waiting_time_in_milliseconds);
+
+      ssize_t send(const char *data, const std::size_t length);
       ssize_t send(const std::string &data, std::size_t length = 0, std::size_t stride = 0);
+
+      bool pending_connection();
 
     protected:
       void print_error(const std::string &message);
@@ -56,9 +65,12 @@ namespace ramrod {
       struct sockaddr_storage *last_receiver_;
       const std::size_t max_buffer_length_;
       bool connected_;
-      short mtu_;
+      uint16_t mtu_;
       bool is_jumbo_packet_;
 
+      bool pending_connection_;
+      std::chrono::duration<long, std::milli> reconnection_time_;
+      std::chrono::time_point<std::chrono::system_clock> *beginning_;
     };
   } // namespace: network_communication
 } // namespace: ramrod
--- src/ramrod/network_communication/server.cpp
+++ src/ramrod/network_communication/server.cpp
@@ -1,6 +1,7 @@
 #include "ramrod/network_communication/server.h"
 
 #include <arpa/inet.h>
+
 #include <cerrno>
 #include <cstdio>
 #include <cstdlib>
@@ -23,16 +24,123 @@ namespace ramrod {
       max_buffer_length_{100},
       connected_{false},
       mtu_{1500},
-      is_jumbo_packet_{false}
+      is_jumbo_packet_{false},
+      pending_connection_{false},
+      reconnection_time_(std::chrono::milliseconds(5000)),
+      beginning_{nullptr}
     {}
 
     server::~server(){
       disconnect();
+      if(beginning_) delete beginning_;
     }
 
     bool server::connect(const std::string ip, const int port){
       ip_ = ip;
       port_ = port;
+      pending_connection_ = true;
+
+      return reconnect();
+      /*
+      rr::attention("Server: waiting for recvfrom...");
+
+      struct sockaddr_storage their_addr;
+      socklen_t addr_len{sizeof(their_addr)};
+      ssize_t num_bytes;
+      char buffer[max_buffer_length_];
+      char s[INET6_ADDRSTRLEN];
+
+      if((num_bytes = ::recvfrom(socket_fd_, buffer, max_buffer_length_ - 1, 0,
+                                (struct sockaddr *)&their_addr, &addr_len)) == -1){
+        ::perror("recvfrom");
+        return false;
+      }
+
+      rr::formatted("Listener: got packet from %s\n", rr::message::message,
+                    ::inet_ntop(their_addr.ss_family,
+                                get_in_addr((struct sockaddr *)&their_addr),
+                                s, sizeof(s)));
+      rr::formatted("Listener: packet is %ld bytes long\n", rr::message::message, num_bytes);
+      buffer[num_bytes] = '\0';
+      rr::formatted("Listened: packet contains \"%s\"\n", buffer);
+      */
+    }
+
+    bool server::disconnect(){
+      if(client_ != nullptr) ::freeaddrinfo(client_);
+
+      if(::close(socket_fd_) < 0){
+        print_error("Connection cannot be closed.");
+        return false;
+      }
+      return !(connected_ = false);
+    }
+
+    uint16_t server::host_to_network(const uint16_t host_value){
+      return ::htons(host_value);
+    }
+
+    uint32_t server::host_to_network(const uint32_t host_value){
+      return ::htonl(host_value);
+    }
+
+    const std::string &server::ip(){
+      return ip_;
+    }
+
+    bool server::is_connected(){
+      return connected_;
+    }
+
+    uint16_t server::mtu(){
+      return mtu_;
+    }
+
+    void server::mtu(const uint16_t mtu_byte_size){
+      mtu_ = mtu_byte_size;
+      is_jumbo_packet_ = mtu_ >= 7700;
+    }
+
+    uint16_t server::network_to_host(const uint16_t network_value){
+      return ::ntohs(network_value);
+    }
+
+    uint32_t server::network_to_host(const uint32_t network_value){
+      return ::ntohl(network_value);
+    }
+
+    int server::port(){
+      return port_;
+    }
+
+    ssize_t server::receive(char *data, const std::size_t length){
+      socklen_t addrlen;
+      ssize_t size{static_cast<ssize_t>(length)};
+      ssize_t total{0};
+      ssize_t bytes_left{size};
+      ssize_t bytes_received;
+
+      while(total < size){
+        bytes_received = ::recvfrom(socket_fd_, data + total,
+                                    static_cast<std::size_t>(bytes_left), 0,
+                                    (struct sockaddr *)last_receiver_, &addrlen);
+        if(bytes_received == 0){
+          print_error("Connection closed when receiving");
+          break;
+        }else if(bytes_received == -1){
+          ::perror("Error while receiving data");
+          break;
+        }
+        total += bytes_received;
+        bytes_left -= bytes_received;
+      }
+
+      return total;
+    }
+
+    bool server::reconnect(){
+      beginning_ =
+          new std::chrono::time_point<std::chrono::system_clock>(std::chrono::system_clock::now());
 
       struct addrinfo hints, *client_info;
       int addrinfo_err;
@@ -45,7 +153,7 @@ namespace ramrod {
       hints.ai_socktype = SOCK_DGRAM;
       hints.ai_flags = AI_PASSIVE;
 
-      if((addrinfo_err = ::getaddrinfo(ip_.c_str(), std::to_string(port).c_str(),
+      if((addrinfo_err = ::getaddrinfo(ip_.c_str(), std::to_string(port_).c_str(),
                                        &hints, &client_info)) != 0){
         formatted("getaddrinfo: %s\n", ::gai_strerror(addrinfo_err));
         return false;
@@ -78,108 +186,30 @@ namespace ramrod {
         print_error("Error: server failed to bind.");
         return connected_ = false;
       }
-      /*
-  rr::attention("Server: waiting for recvfrom...");
 
-  struct sockaddr_storage their_addr;
-  socklen_t addr_len{sizeof(their_addr)};
-  ssize_t num_bytes;
-  char buffer[max_buffer_length_];
-  char s[INET6_ADDRSTRLEN];
-
-  if((num_bytes = ::recvfrom(socket_fd_, buffer, max_buffer_length_ - 1, 0,
-                            (struct sockaddr *)&their_addr, &addr_len)) == -1){
-    ::perror("recvfrom");
-    return false;
-  }
-
-  rr::formatted("Listener: got packet from %s\n", rr::message::message,
-                ::inet_ntop(their_addr.ss_family,
-                            get_in_addr((struct sockaddr *)&their_addr),
-                            s, sizeof(s)));
-  rr::formatted("Listener: packet is %ld bytes long\n", rr::message::message, num_bytes);
-  buffer[num_bytes] = '\0';
-  rr::formatted("Listened: packet contains \"%s\"\n", buffer);
-  */
-      return connected_ = true;
+      delete beginning_;
+      return connected_ = !(pending_connection_ = false);
     }
 
-    bool server::disconnect(){
-      if(client_ != nullptr) ::freeaddrinfo(client_);
-
-      if(::close(socket_fd_) < 0){
-        print_error("Connection cannot be closed.");
-        return false;
-      }
-      return !(connected_ = false);
+    int server::time_to_reconnect(){
+      return static_cast<int>(reconnection_time_.count());
     }
 
-    uint16_t server::host_to_network(const uint16_t host_value){
-      return ::htons(host_value);
+    void server::time_to_reconnect(const int waiting_time_in_milliseconds){
+      if(waiting_time_in_milliseconds > 0)
+        reconnection_time_ = std::chrono::duration<long, std::milli>(
+              std::chrono::milliseconds(waiting_time_in_milliseconds)
+            );
     }
 
-    uint32_t server::host_to_network(const uint32_t host_value){
-      return ::htonl(host_value);
-    }
-
-    const std::string &server::ip(){
-      return ip_;
-    }
-
-    short server::mtu(){
-      return mtu_;
-    }
-
-    void server::mtu(const short mtu_byte_size){
-      mtu_ = mtu_byte_size;
-      is_jumbo_packet_ = mtu_ >= 7700;
-    }
-
-    uint16_t server::network_to_host(const uint16_t network_value){
-      return ::ntohs(network_value);
-    }
-
-    uint32_t server::network_to_host(const uint32_t network_value){
-      return ::ntohl(network_value);
-    }
-
-    int server::port(){
-      return port_;
-    }
-
-    ssize_t server::receive(void *data, const std::size_t length){
-      socklen_t addrlen;
-      ssize_t size{static_cast<ssize_t>(length)};
-      ssize_t total{0};
-      ssize_t bytes_left{size};
-      ssize_t bytes_received;
-
-      while(total < size){
-        bytes_received = ::recvfrom(socket_fd_, reinterpret_cast<char*>(data) + total,
-                                    static_cast<std::size_t>(bytes_left), 0,
-                                    (struct sockaddr *)last_receiver_, &addrlen);
-        if(bytes_received == 0){
-          print_error("Connection closed when receiving");
-          break;
-        }else if(bytes_received == -1){
-          ::perror("Error while receiving data");
-          break;
-        }
-        total += bytes_received;
-        bytes_left -= bytes_received;
-      }
-
-      return bytes_received = total;
-    }
-
-    ssize_t server::send(const void *data, const std::size_t length){
+    ssize_t server::send(const char *data, const std::size_t length){
       ssize_t size{static_cast<ssize_t>(length)};
       ssize_t total{0};
       ssize_t bytes_left{size};
       ssize_t bytes_sent;
 
       while(total < size){
-        bytes_sent = ::sendto(socket_fd_, reinterpret_cast<const char*>(data) + total,
+        bytes_sent = ::sendto(socket_fd_, data + total,
                               static_cast<std::size_t>(bytes_left), MSG_DONTROUTE,
                               client_->ai_addr, client_->ai_addrlen);
         if(bytes_sent == 0){
@@ -193,7 +223,15 @@ namespace ramrod {
         bytes_left -= bytes_sent;
       }
 
-      return bytes_sent = total;
+      return total;
+    }
+
+    bool server::pending_connection(){
+      if(!pending_connection_) return false;
+
+      if((std::chrono::system_clock::now() - *beginning_) < reconnection_time_) return true;
+
+      return !reconnect();
     }
 
     // ::::::::::::::::::::::::::::::::::: PROTECTED FUNCTIONS :::::::::::::::::::::::::::::::::::
