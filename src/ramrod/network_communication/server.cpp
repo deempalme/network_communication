78a1628b5b84fc3da365fc9721e476e9a1d85bf0
deempalme <20066249+deempalme@users.noreply.github.com>
2021-03-05 18:30:16 -0700

    Adding multithreading network communication

--- src/ramrod/network_communication/server.cpp
+++ src/ramrod/network_communication/server.cpp
@@ -1,86 +1,69 @@
 #include "ramrod/network_communication/server.h"
 
-#include <arpa/inet.h>
+#include <cerrno>                      // for errno
+#include <cstring>                     // for memset
+#include <iosfwd>                      // for size_t
+#include <netdb.h>                     // for addrinfo, freeaddrinfo, gai_st...
+#include <netinet/in.h>                // for htonl, htons, ntohl, ntohs
+#include <signal.h>                    // for sigaction, sigemptyset, SA_RES...
+#include <sys/wait.h>                  // for waitpid, WNOHANG
+#include <thread>                      // for sleep_for, thread
+#include <unistd.h>                    // for ssize_t, close
 
-#include <cerrno>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <iostream>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "ramrod/console.h"            // for formatted
+#include "ramrod/console/attention.h"  // for attention_stream, attention
+#include "ramrod/console/endl.h"       // for endl
+#include "ramrod/console/error.h"      // for error, error_stream
+#include "ramrod/console/perror.h"     // for perror, perror_stream
+#include "ramrod/console/types.h"      // for error
+#include "ramrod/console/warning.h"    // for warning, warning_stream
 
 namespace ramrod {
   namespace network_communication {
     server::server() :
       ip_(),
       port_{1313},
-      socket_fd_{0},
-      client_{nullptr},
-      last_receiver_{nullptr},
-      max_buffer_length_{100},
+      socket_fd_{-1},
+      connected_fd_{-1},
+      max_queue_{10},
+      current_intent_{0},
+      max_intents_{10},
+      terminate_concurrent_{false},
+      terminate_receive_{false},
+      terminate_send_{false},
       connected_{false},
-      mtu_{1500},
-      is_jumbo_packet_{false},
-      pending_connection_{false},
-      reconnection_time_(std::chrono::milliseconds(5000)),
-      beginning_{nullptr}
+      connecting_{false},
+      reconnection_time_(std::chrono::milliseconds(5000))
     {}
 
     server::~server(){
       disconnect();
-      if(beginning_) delete beginning_;
     }
 
-    bool server::connect(const std::string ip, const int port){
+    void server::connect(const std::string ip, const int port){
+      if(connected_) disconnect();
+
       ip_ = ip;
       port_ = port;
-      pending_connection_ = true;
+      connecting_ = true;
+      current_intent_ = 0;
 
-      return reconnect();
-      /*
-      rr::attention("Server: waiting for recvfrom...");
-
-      struct sockaddr_storage their_addr;
-      socklen_t addr_len{sizeof(their_addr)};
-      ssize_t num_bytes;
-      char buffer[max_buffer_length_];
-      char s[INET6_ADDRSTRLEN];
-
-      if((num_bytes = ::recvfrom(socket_fd_, buffer, max_buffer_length_ - 1, 0,
-                                (struct sockaddr *)&their_addr, &addr_len)) == -1){
-        ::perror("recvfrom");
-        return false;
-      }
-
-      rr::formatted("Listener: got packet from %s\n", rr::message::message,
-                    ::inet_ntop(their_addr.ss_family,
-                                get_in_addr((struct sockaddr *)&their_addr),
-                                s, sizeof(s)));
-      rr::formatted("Listener: packet is %ld bytes long\n", rr::message::message, num_bytes);
-      buffer[num_bytes] = '\0';
-      rr::formatted("Listened: packet contains \"%s\"\n", buffer);
-      */
+      std::thread(&server::concurrent_connector, this, true).detach();
     }
 
     bool server::disconnect(){
-      if(client_ != nullptr) ::freeaddrinfo(client_);
+      terminate_send_ = true;
+      terminate_receive_ = true;
+      terminate_concurrent_ = true;
 
-      if(::close(socket_fd_) < 0){
-        print_error("Connection cannot be closed.");
-        return false;
-      }
-      return !(connected_ = false);
+      return close_child() & close();
     }
 
-    uint16_t server::host_to_network(const uint16_t host_value){
+    std::uint16_t server::host_to_network(const std::uint16_t host_value){
       return ::htons(host_value);
     }
 
-    uint32_t server::host_to_network(const uint32_t host_value){
+    std::uint32_t server::host_to_network(const std::uint32_t host_value){
       return ::htonl(host_value);
     }
 
@@ -92,20 +75,29 @@ namespace ramrod {
       return connected_;
     }
 
-    uint16_t server::mtu(){
-      return mtu_;
+    int server::max_queue(){
+      return max_queue_;
     }
 
-    void server::mtu(const uint16_t mtu_byte_size){
-      mtu_ = mtu_byte_size;
-      is_jumbo_packet_ = mtu_ >= 7700;
+    bool server::max_queue(const int new_max_queue){
+      if(new_max_queue <= 0) return false;
+      max_queue_ = new_max_queue;
+      return true;
     }
 
-    uint16_t server::network_to_host(const uint16_t network_value){
+    std::uint32_t server::max_reconnection_intents(){
+      return max_intents_;
+    }
+
+    void server::max_reconnection_intents(const std::uint32_t new_max_intents){
+      max_intents_ = new_max_intents;
+    }
+
+    std::uint16_t server::network_to_host(const std::uint16_t network_value){
       return ::ntohs(network_value);
     }
 
-    uint32_t server::network_to_host(const uint32_t network_value){
+    std::uint32_t server::network_to_host(const std::uint32_t network_value){
       return ::ntohl(network_value);
     }
 
@@ -113,82 +105,93 @@ namespace ramrod {
       return port_;
     }
 
-    ssize_t server::receive(char *data, const std::size_t length){
-      socklen_t addrlen;
-      ssize_t size{static_cast<ssize_t>(length)};
-      ssize_t total{0};
-      ssize_t bytes_left{size};
-      ssize_t bytes_received;
-
-      while(total < size){
-        bytes_received = ::recvfrom(socket_fd_, data + total,
-                                    static_cast<std::size_t>(bytes_left), 0,
-                                    (struct sockaddr *)last_receiver_, &addrlen);
-        if(bytes_received == 0){
-          print_error("Connection closed when receiving");
-          break;
-        }else if(bytes_received == -1){
-          ::perror("Error while receiving data");
-          break;
-        }
-        total += bytes_received;
-        bytes_left -= bytes_received;
+    ssize_t server::receive(char *buffer, const ssize_t size, const int flags){
+      if(!connected_){
+        rr::error("There is no connection to receive from.");
+        return -1;
       }
 
-      return total;
+      ssize_t total_received{0};
+      ssize_t bytes_left = size;
+      ssize_t received_size;
+      std::uint32_t error_counter{0};
+
+      while(total_received < size){
+        if((received_size = ::recv(connected_fd_, buffer + total_received,
+                                   static_cast<std::size_t>(bytes_left), flags)) == -1){
+          rr::perror("Receiving data");
+          std::this_thread::sleep_for(std::chrono::milliseconds(5));
+          if(++error_counter > max_intents_){
+            total_received = -1;
+            break;
+          }
+        }
+        total_received += received_size;
+        bytes_left -= received_size;
+      }
+      return total_received;
     }
 
-    bool server::reconnect(){
-      beginning_ =
-          new std::chrono::time_point<std::chrono::system_clock>(std::chrono::system_clock::now());
-
-      struct addrinfo hints, *client_info;
-      int addrinfo_err;
-      int yes = 1;
-
-      // first, load up address structs with getaddrinfo():
-
-      std::memset(&hints, 0, sizeof(hints));
-      hints.ai_family = AF_UNSPEC;
-      hints.ai_socktype = SOCK_DGRAM;
-      hints.ai_flags = AI_PASSIVE;
-
-      if((addrinfo_err = ::getaddrinfo(ip_.c_str(), std::to_string(port_).c_str(),
-                                       &hints, &client_info)) != 0){
-        formatted("getaddrinfo: %s\n", ::gai_strerror(addrinfo_err));
+    bool server::receive_concurrently(char *buffer, ssize_t *size, const int flags){
+      if(!connected_){
+        rr::error("There is no connection to receive from.");
+        *size = -1;
         return false;
       }
 
-      // loop through all the results and bind to the first we can
-      for(client_ = client_info; client_ != nullptr; client_ = client_->ai_next){
-        if((socket_fd_ = ::socket(client_->ai_family, client_->ai_socktype,
-                                  client_->ai_protocol)) == -1){
-          ::perror("Error selecting socket");
-          continue;
-        }
+      std::thread(&server::concurrent_receive, this, buffer, size, flags).detach();
+      return true;
+    }
 
-        if(::setsockopt(socket_fd_, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1){
-          ::perror("Error setting socket options");
-          return false;
-        }
+    bool server::reconnect(){
+      if(connecting_ || ip_.size() == 0 || port_ <= 0) return false;
 
-        if(::bind(socket_fd_, client_->ai_addr, client_->ai_addrlen) == -1){
-          ::close(socket_fd_);
-          ::perror("Error binding socket");
-          return false;
-        }
-        break;
+      if(connected_) disconnect();
+
+      connecting_ = true;
+      current_intent_ = 0;
+
+      std::thread(&server::concurrent_connector, this, true).detach();
+
+      return true;
+    }
+
+    ssize_t server::send(const char *buffer, const ssize_t size, const int flags){
+      if(!connected_){
+        rr::error("There is no connection to send from.");
+        return -1;
       }
 
-      ::freeaddrinfo(client_info); // all done with this structure
+      ssize_t total_sent{0};
+      ssize_t bytes_left = size;
+      ssize_t sent_size;
+      std::uint32_t error_counter{0};
 
-      if(client_ == nullptr){
-        print_error("Error: server failed to bind.");
-        return connected_ = false;
+      while(total_sent < size){
+        if((sent_size = ::send(connected_fd_, buffer + total_sent,
+                               static_cast<std::size_t>(bytes_left), flags)) == -1){
+          rr::perror("Sending data");
+          std::this_thread::sleep_for(std::chrono::milliseconds(5));
+          if(++error_counter > max_intents_){
+            total_sent = -1;
+            break;
+          }
+        }
+        total_sent += sent_size;
+        bytes_left -= sent_size;
+      }
+      return total_sent;
+    }
+
+    bool server::send_concurrently(const char *buffer, ssize_t *size, const int flags){
+      if(!connected_){
+        rr::error("There is no connection to send from.");
+        *size = -1;
+        return false;
       }
 
-      delete beginning_;
-      return connected_ = !(pending_connection_ = false);
+      std::thread(&server::concurrent_send, this, buffer, size, flags).detach();
+      return true;
     }
 
     int server::time_to_reconnect(){
@@ -202,65 +205,200 @@ namespace ramrod {
             );
     }
 
-    ssize_t server::send(const char *data, const std::size_t length){
-      ssize_t size{static_cast<ssize_t>(length)};
-      ssize_t total{0};
-      ssize_t bytes_left{size};
-      ssize_t bytes_sent;
-
-      while(total < size){
-        bytes_sent = ::sendto(socket_fd_, data + total,
-                              static_cast<std::size_t>(bytes_left), MSG_DONTROUTE,
-                              client_->ai_addr, client_->ai_addrlen);
-        if(bytes_sent == 0){
-          print_error("Connection closed when sending");
-          break;
-        }else if(bytes_sent == -1){
-          ::perror("Error while sending data");
-          break;
-        }
-        total += bytes_sent;
-        bytes_left -= bytes_sent;
-      }
-
-      return total;
-    }
-
-    bool server::pending_connection(){
-      if(!pending_connection_) return false;
-
-      if((std::chrono::system_clock::now() - *beginning_) < reconnection_time_) return true;
-
-      return !reconnect();
-    }
-
-    // ::::::::::::::::::::::::::::::::::: PROTECTED FUNCTIONS :::::::::::::::::::::::::::::::::::
-
-    void server::print_error(const std::string &message){
-      std::cout << "\033[1;41m Error: \033[0;1;38;5;174m " << message << "\033[0m" << std::endl;
-    }
-
-    int server::formatted(const char *format, ...){
-      std::cout << "\033[0;1;38;5;174m";
-
-      va_list arg;
-      va_start(arg, format);
-      const int result{std::vprintf(format, arg)};
-      va_end(arg);
-
-      std::cout << "\033[0m";
-      std::flush(std::cout);
-
-      return result;
-    }
-
     // :::::::::::::::::::::::::::::::::::: PRIVATE FUNCTIONS ::::::::::::::::::::::::::::::::::::
 
-    void *server::get_in_addr(sockaddr *sa){
-      // get sockaddr, IPv4 or IPv6:
-      if(sa->sa_family == AF_INET)
-        return &(((struct sockaddr_in *)sa)->sin_addr);
-      return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+    bool server::close(){
+      if(socket_fd_ < 0){
+        rr::warning("Connection already closed.");
+        return true;
+      }
+
+      if(::close(socket_fd_) == -1){
+        rr::perror("Connection cannot be closed");
+        return false;
+      }
+
+      socket_fd_ = -1;
+      return true;
+    }
+
+    bool server::close_child(){
+      if(connected_fd_ < 0){
+        rr::warning("Children connection already closed.");
+        return !(connected_ = false);
+      }
+
+      if(::close(connected_fd_) == -1){
+        rr::perror("Children connection cannot be closed");
+        return false;
+      }
+
+      connected_ = false;
+      connected_fd_ = -1;
+      return true;
+    }
+
+    void server::concurrent_connector(const bool force){
+      if(!force && terminate_concurrent_){
+        terminate_concurrent_ = false;
+        return;
+      }
+      if(connected_) return;
+
+      int status;
+      struct addrinfo hints;
+      struct addrinfo *results; // Will point to the results
+      struct addrinfo *pointer;
+
+      std::memset(&hints, 0, sizeof(hints));  // make sure the struct is empty
+      hints.ai_family = AF_UNSPEC;            // don't care if IPv4 or IPv6
+      hints.ai_socktype = SOCK_STREAM;        // TCP stream sockets
+      hints.ai_flags = AI_PASSIVE;            // Fill in my Ip information for me
+
+      // first, load up address structs with getaddrinfo():
+      if((status = ::getaddrinfo(ip_.c_str(), std::to_string(port_).c_str(),
+                                 &hints, &results)) != 0){
+        rr::formatted("Error: getaddrinfo (%s)\n", rr::message::error, ::gai_strerror(status));
+        return;
+      }
+
+      // loop through all the results and bind to the first we can
+      for(pointer = results; pointer != nullptr; pointer = pointer->ai_next){
+        // Making a socket
+        if((socket_fd_ = ::socket(pointer->ai_family, pointer->ai_socktype,
+                                  pointer->ai_protocol)) == -1){
+          rr::perror("Selecting socket");
+          continue;
+        }
+
+        if(::setsockopt(socket_fd_, SOL_SOCKET, SO_REUSEADDR, &status, sizeof(int)) == -1){
+          rr::perror("Setting socket options");
+          ::close(socket_fd_);
+          return;
+        }
+
+        // Binding the socket to the port
+        if(::bind(socket_fd_, pointer->ai_addr, pointer->ai_addrlen) == -1){
+          ::close(socket_fd_);
+          rr::perror("Binding socket");
+          continue;
+        }
+        break;
+      }
+
+      ::freeaddrinfo(results); // all done with this structure
+
+      if(pointer == nullptr){
+        rr::perror("Server failed to bind");
+        if(++current_intent_ > max_intents_){
+          rr::error("Max number of reconnections has been reached "
+                    "and therefore failed to connect.");
+          return;
+        }
+        rr::attention() << "Trying reconnection in " << reconnection_time_.count() / 1000
+                        << " seconds... (#" << current_intent_ << ")" << rr::endl;
+        std::this_thread::sleep_for(reconnection_time_);
+        std::thread(&server::concurrent_connector, this, false).detach();
+        return;
+      }
+
+      if(::listen(socket_fd_, max_queue_) == -1){
+        rr::perror("Listening to socket");
+        return;
+      }
+
+      struct sigaction signal_action;
+
+      // Reaping all dead processes
+      signal_action.sa_handler = signal_children_handler;
+      ::sigemptyset(&signal_action.sa_mask);
+      signal_action.sa_flags = SA_RESTART;
+      if(::sigaction(SIGCHLD, &signal_action, nullptr) == -1){
+        rr::perror("Reaping dead processes");
+        return;
+      }
+
+      std::thread(&server::concurrent_connection, this, socket_fd_).detach();
+    }
+
+    void server::concurrent_connection(const int socket_fd){
+      rr::attention("Waiting for incomming connections!");
+
+      struct sockaddr_storage their_addr;
+      socklen_t addr_size;
+      int accepted_fd{0};
+
+      while(!terminate_concurrent_){
+        addr_size = sizeof(their_addr);
+        // Accept incoming connection
+        if((accepted_fd = ::accept(socket_fd, (struct sockaddr*)&their_addr, &addr_size)) == -1){
+          rr::perror("Accepting connection");
+          continue;
+        }
+
+        rr::attention("Connection established!");
+
+        connected_ = true;
+        connecting_ = false;
+        connected_fd_ = accepted_fd;
+
+        // Listening socket not needed anymore
+        close();
+        break;
+      }
+    }
+
+    void server::concurrent_receive(char *buffer, ssize_t *size, const int flags){
+      ssize_t total_received{0};
+      ssize_t bytes_left = *size;
+      ssize_t received_size;
+      std::uint32_t error_counter{0};
+
+      while(total_received < *size && !terminate_send_){
+        if((received_size = ::recv(connected_fd_, buffer + total_received,
+                                   static_cast<std::size_t>(bytes_left), flags)) == -1){
+          rr::perror("Receiving data");
+          std::this_thread::sleep_for(std::chrono::milliseconds(5));
+          if(++error_counter > max_intents_){
+            total_received = -1;
+            break;
+          }
+        }
+        total_received += received_size;
+        bytes_left -= received_size;
+      }
+      *size = total_received;
+    }
+
+    void server::concurrent_send(const char *buffer, ssize_t *size, const int flags){
+      ssize_t total_sent{0};
+      ssize_t bytes_left = *size;
+      ssize_t sent_size;
+      std::uint32_t error_counter{0};
+
+      while(total_sent < *size && !terminate_send_){
+        if((sent_size = ::send(connected_fd_, buffer + total_sent,
+                               static_cast<std::size_t>(bytes_left), flags)) == -1){
+          rr::perror("Sending data");
+          std::this_thread::sleep_for(std::chrono::milliseconds(5));
+          if(++error_counter > max_intents_){
+            total_sent = -1;
+            break;
+          }
+        }
+        total_sent += sent_size;
+        bytes_left -= sent_size;
+      }
+      *size = total_sent;
+    }
+
+    // :::::::::::::::::::::::::::::::::::: OUTTER FUNCTIONS :::::::::::::::::::::::::::::::::::
+
+    void signal_children_handler(const int /*signal*/){
+      // waitpid() might overwrite errno, so we save and restore it:
+      const int saved_errno = errno;
+      while(::waitpid(-1, nullptr, WNOHANG) > 0);
+      errno = saved_errno;
     }
   } // namespace: network_communication
 } // namespace: ramrod
